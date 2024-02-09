d7627c0bfed40c9144030c5c772a8b081c964459
PardhuVuppala <106818434+PardhuVuppala@users.noreply.github.com>
2024-02-09 17:46:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = 1200;
 
 app.use(express.json());
 app.use(cors());
-app.use("/Api",require('./Routes/Api'))
+app.use("/api",require('./Routes/Api'))
 
 app.listen(port, () => {
   console.log(`server is running in port ${port}`);
