df485d478d1b57d7c3539de4a08c84104f66c4be
Kishore29-02 <kishore01.official@gmail.com>
2024-02-14 19:16:27 +0530

    Router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import Footer from "./components/Footer";
-// import Card from "./components/Card";
-import RecipeContainer from "./components/RecipeContainer";
-
-import {getSavedRecipesById} from './api';
+import {Routes, Route} from "react-router-dom"
+import SavedRecipe from "./Pages/SavedRecipe";
 
 function App() {
-  const [recipes,setRecipes] = React.useState([]);
-
-  const getRecipes = async () => {
-    const res = await getSavedRecipesById('b7878c9e-12b4-44bd-8eac-c0556ae67bef');
-    setRecipes(res);
-  }
-  React.useEffect(() => {
-    getRecipes()
-  },[]);
-
-  // debugger;
-
   return (
-    <div className="App">
-      <Header />
-      <div>
-        <Search />
-        <RecipeContainer
-          data = {recipes.data.recipe}
-        />
-      </div>    
-      <Footer />
-    </div>
+    <Routes >
+      <Route path="/api/:user_id/saved-recipes" element={<SavedRecipe/>}/>
+    </Routes>
   );
 }
 
---
+++ client/src/Pages/SavedRecipe.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Header from "../components/Header";
+import Search from "../components/Search";
+import Footer from "../components/Footer";
+import RecipeContainer from "../components/RecipeContainer";
+
+import {getSavedRecipesById} from '../api';
+
+function SavedRecipe() {
+  const [recipes,setRecipes] = React.useState([]);
+
+  const getRecipes = async () => {
+    const res = await getSavedRecipesById('b7878c9e-12b4-44bd-8eac-c0556ae67bef');
+    setRecipes(res.data.recipe);
+  }
+  React.useEffect(() => {
+    getRecipes()
+  },[]);
+
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <Search />
+        <RecipeContainer
+          data = {recipes}
+        />
+      </div>    
+      <Footer />
+    </div>
+  );
+}
+
+export default SavedRecipe;
