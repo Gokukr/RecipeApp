fe39bbd9aa3eeb657dbfb1b155df72151f5651a0
VISHAL KUMAR SINGH <100745451+vishalkr1030@users.noreply.github.com>
2024-02-17 19:29:33 +0530

    Update Rating.jsx

--- client/src/components/Rating.jsx
+++ client/src/components/Rating.jsx
@@ -1,30 +1,48 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import { response } from 'express';
 
-function Rating({ recipeId, initialAverageRating, total_ratings }) {
-  // total_ratings=total_ratings+1;
-  // recipeId='8b66b170-321d-4b45-8e6d-1cb53221fa11';
-  // initialAverageRating=5;
+function Rating({ recipeId }) {
   const [selectedRating, setSelectedRating] = useState(0); // Adjust based on user's previous rating
 
   const handleRatingChange = (newRating) => {
     setSelectedRating(newRating);
   };
+  const [initialAverageRating, setRating]=useState(null);
+  const [total_ratings, setTotal]=useState(0);
+  console.log(recipeId);
+  useEffect(() => {
+    axios.get(`http://localhost:1200/api/detail/recipes/${recipeId}`)
+    .then((response) => {
+      setRating(response.data.rating);
+    });
+      
+  }, [recipeId]);
 
+  useEffect(() => {
+    axios.get(`http://localhost:1200/api/detail/recipes/${recipeId}`)
+    .then((response) => {
+      setTotal(response.data.count);
+    });
+      
+  }, [recipeId]);
+  
+  // const total_ratings=2;
   const submitRating = async () => {
-    const Rate = total_ratings * initialAverageRating;
-    const total = total_ratings + 1;
-    const newRating = (Rate + selectedRating) / total;
-    console.log("api hit update api");
-    axios
-      .put(
-        `http://localhost:1200/api/detail/recipe/update/${recipeId}/${newRating}/${total}`
-      )
+    const Rate = Number(total_ratings) * Number(initialAverageRating);
+    console.log(total_ratings);
+    console.log(initialAverageRating);
+    const total = Number(total_ratings) + Number(1);
+    // const total=3;
+    // console.log(total);
+    const newRating = (Rate+selectedRating)/(total);
+    // console.log(newRating);
+    axios.put(`http://localhost:1200/api/detail/recipe/update/${recipeId}/${newRating}/${total}`)
       .then(() => {
-        console.log("Review Submitted");
+        console.log('Review Submitted');
       })
       .catch((error) => {
-        console.error("Error in submitting review", error);
+        console.error('Error in submitting review', error);
       });
   };
 
@@ -36,19 +54,20 @@ function Rating({ recipeId, initialAverageRating, total_ratings }) {
       <div className="flex space-x-1">
         {ratingStars.map((rating) => (
           <span
-            key={rating}
-            onClick={() => handleRatingChange(rating)}
-            className="cursor-pointer text-3xl hover:text-yellow-200"
-          >
-            <svg width="30" height="30">
-              <circle
-                cx="15"
-                cy="15"
-                r="10"
-                fill={rating <= selectedRating ? "#FFFF00" : "#ffffff"}
-              />
-            </svg>
-          </span>
+          key={rating}
+          onClick={() => handleRatingChange(rating)}
+          className="cursor-pointer text-3xl hover:text-yellow-200"
+        >
+          <svg width="30" height="30">
+            <circle
+              cx="15"
+              cy="15"
+              r="10"
+              fill={rating <= selectedRating ? "#FFFF00" : "#ffffff"}
+            />
+          </svg>
+        </span>
+        
         ))}
       </div>
       <p className="mt-5 text-sm">Selected Rating: {selectedRating}</p>
