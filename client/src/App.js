0c6242a14dad1da3ba60d9d1f813f86f9400093e
Kishore29-02 <kishore01.official@gmail.com>
2024-03-01 14:13:32 +0530

    cmt: button corrected

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Login from "./components/Login.jsx";
 import SignUp from "./components/SignUp.jsx";
 import "./index.css";
 import Dashboard from "./components/Dashboard.jsx";
-import Footer from "./components/Footer.jsx";
+// import Footer from "./components/Footer.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserProfile from "./components/UserProfile.jsx";
 import DetailRecipe from "./components/DetailRecipe.jsx";
 import SavedRecipe from "./components/SavedRecipe.jsx";
 import AddRecipe from "./components/AddRecipe.jsx";
 import UpdateRecipe from "./components/UpdateRecipe.jsx";
+import RecipeRequest from "./components/RecipeRequest.jsx";
+
 function App() {
   return (
     <div>
@@ -19,6 +21,7 @@ function App() {
           <Route path="/SignUp" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/user" element={<UserProfile />} />
+          <Route path="/recipe-request" element={<RecipeRequest/>}/>
           <Route
             path="user/detail-recipe/:recipeId"
             element={<DetailRecipe />}
