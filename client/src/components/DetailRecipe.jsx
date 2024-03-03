639693313f39b5540f19a86776bf603b12c142c0
VISHAL KUMAR SINGH <100745451+vishalkr1030@users.noreply.github.com>
2024-03-03 20:41:32 +0530

    Update DetailRecipe.jsx, updated rating Stars to show partial rating

--- client/src/components/DetailRecipe.jsx
+++ client/src/components/DetailRecipe.jsx
@@ -29,7 +29,7 @@ function Detailrecipe() {
     }
     
   },[role, userRole,recipeId,userId]);
-  const [recipe, setRecipe] = useState(null);
+  const [recipe, setRecipe] = useState([]);
   useEffect(() => {
     axios
       .get(`http://localhost:1200/api/detail/recipes/${recipeId}`)
@@ -80,6 +80,13 @@ function Detailrecipe() {
     window.location.reload();
   };
 
+  const [percentage, setPercentage] = useState("");
+  useEffect(() => {
+    const rate = Math.floor(recipe.rating);
+    const part = ((recipe.rating) - rate)*100;
+    setPercentage(Math.floor(part)+'%');
+  },[recipe.rating]);
+
   return (
     <div class="bg-white">
       <Header />
@@ -88,7 +95,64 @@ function Detailrecipe() {
           <div>
             <h2 class="font-bold ml-3 text-[60px]">{recipe.title}</h2>
             <div className="rating-container flex justify-between mt-[-10px]">
-            <p class="rating-container-rating-tab  pl-5 text-[20px] font-bold tracking-wide">Rating: {recipe.rating}</p>
+            <div class="rating-container-rating-tab flex flex-row mt-0 justify-start items-center">
+            <p class="pl-5 text-[20px] font-bold tracking-wide">Rating: {recipe.rating}</p>
+            <div className="pl-5 tracking-wide pt-1">
+                  {Array.from(
+                    { length: Math.floor(recipe.rating) },
+                    (_, index) => (
+                      <svg
+                        key={index}
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-star-fill text-yellow-500 pl-2"
+                        viewBox="0 0 16 17"
+                        stroke="black"
+                        stroke-width="1"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        
+                      </svg>
+                      
+                    )
+                  )}
+                  
+                <svg width="16" height="16" viewBox="0 0 16 17" className="pl-2">
+                  <defs>
+                      <linearGradient id="half">
+                          <stop offset={percentage} stop-color="rgb(234 179 8)" />
+                          <stop offset={percentage} stop-color="white" />
+                      </linearGradient>            
+                  </defs>
+                  <g fill="url(#half)" stroke="black" stroke-width="1">
+                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </g>
+                </svg>
+                  {Array.from(
+                    { length: (4-Math.floor(recipe.rating)) },
+                    (_, index) => (
+                      <svg
+                        key={index}
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-star-fill text-white pl-2"
+                        viewBox="0 0 16 17"
+                        stroke="black"
+                        stroke-width="1"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        
+                      </svg>
+                      
+                    )
+                  )}
+
+                
+
+            </div>
+            </div>
             
             <button
               onClick={handleAddToFavourites}
