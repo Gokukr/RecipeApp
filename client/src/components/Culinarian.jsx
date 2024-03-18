a1a5b334c2424dd3d22f3d55220c0085e6f88180
VISHAL KUMAR SINGH <100745451+vishalkr1030@users.noreply.github.com>
2024-03-18 14:26:09 +0530

    Update Culinarian.jsx

--- client/src/components/Culinarian.jsx
+++ client/src/components/Culinarian.jsx
@@ -28,15 +28,18 @@ function Culinarian() {
       });
   }, [navigate]);
 
+  //To accept the culinarian request
   function handleAccept() {
     setStatus("Accept");
     setShowModal(true);
   }
+  //To reject the culinarian request
   function handleReject() {
     setStatus("Reject");
     setShowModal(true);
   }
 
+  //To set color according to status
   const getTagClass = (status) => {
     switch (status) {
       case "Accepted":
@@ -50,6 +53,7 @@ function Culinarian() {
     }
   };
 
+  //To display requests
   const RequestCard = ({ name, id, user_id, specialization, bio, status }) => (
     <div
       className="flex justify-between items-center px-4 gap-4 py-4 min-h-40 lg:w-full bg-white hover:drop-shadow-2xl rounded-md text-darkslategray-100 md:w-5/6 sm:w-11/12"
@@ -63,33 +67,20 @@ function Culinarian() {
         <div className="w-[30%] flex flex-col justify-between items-center">
           <p className="text-[18px] font-bold text-gray-900">Specialization</p>
           <p className="text-[18px] font-bold text-gray-900 pt-2">
-            {specialization}
-          </p>
-        </div>
-        <div className="w-[30%] flex flex-col  justify-between items-center">
-          <p className="text-[18px] font-bold text-gray-900">Bio</p>
-          <p className="text-[18px] font-bold text-gray-900 pt-2">{bio}</p>
-        </div>
-        {/* <div className="w-[30%] text-[18px] font-bold text-gray-900 flex flex-col gap-3 justify-between items-center px-2">
-          <span> Specialization</span>
-          <span>
-            <div class="container flex flex-col justify-between items-center pt-2">
-              {Array.isArray(specialization) ? (
+            {Array.isArray(specialization) ? (
                 specialization.map((Specialization, index) => (
                   <span key={index}>{Specialization}</span>
                 ))
               ) : (
                 <span>{specialization}</span>
               )}
-            </div>
-          </span>
-        </div>
-        <div className="w-[40%] flex flex-col px-2 gap-3 justify-center items-center ">
-          <p className="text-[18px] font-bold text-gray-900 ">Bio</p>
-          <p className="text-[18px] font-bold text-gray-900 flex flex-wrap break-words">
-            {bio}
           </p>
-        </div> */}
+        </div>
+        <div className="w-[30%] flex flex-col  justify-between items-center">
+          <p className="text-[18px] font-bold text-gray-900">Bio</p>
+          <p className="text-[18px] font-bold text-gray-900 pt-2">{bio}</p>
+        </div>
+        
       </div>
 
       <div
@@ -175,16 +166,7 @@ function Culinarian() {
 
           {hasData === true && (
             <>
-              {/* flex justify-between items-center px-4 gap-5 py-4 min-h-40 lg:w-full bg-white hover:drop-shadow-2xl rounded-md text-darkslategray-100 md:w-5/6 sm:w-11/12 */}
-              {/* <div className='lg:w-full flex flex-row justify-between items-center px-4 gap-5 py-4 bg-primary-100 hover:drop-shadow-2xl rounded-md text-xl md:w-5/6 sm:w-11/12 font-bold text-white'>
-                    <div className='outer w-[60%] flex justify-center px-2 items-center gap-3'>
-                    <div className="name w-[30%] ">Name</div>
-                    <div className="specialization w-[30%] ">Specialization</div>
-                    <div className="bio w-[40%]">Bio</div>
-                    </div>
-                    <div className="stat w-[20%]">Status</div>
-                    <div className="action w-[10%]">Action</div>
-                </div> */}
+              
               <div>
                 {Array.isArray(culinaryData) ? (
                   culinaryData.map((data, index) => (
@@ -204,75 +186,6 @@ function Culinarian() {
                     </div>
                   ))
                 ) : (
-                  //   culinaryData.map((data, index) => (
-                  //     <li key={index} class="flex justify-between bg-gray-100 shadow-4xl shadow-primary-100 place-items-start px-4 gap-4 py-4 mb-5 border-2 border-solid border-black rounded-xl">
-                  //         {/* <span>{index+1}.</span> */}
-                  //         <div class="flex flex-col gap-3 justify-between items-center  w-[25%] ">
-                  //             <span> Name</span>
-                  //             <span class="pt-2 text-[18px]">{ data.f_name } {data.l_name}</span>
-                  //         </div>
-                  //         <div class="flex flex-col gap-3 justify-between items-center w-[25%]">
-                  //             <span> Specialization</span>
-                  //         <span class="pt-2 text-[18px]">
-                  //         <div class="container flex flex-col justify-between items-center">
-                  //             {Array.isArray(data.specialization) ? (
-                  //                 data.specialization.map((Specialization, index) => (
-                  //                 <span key={index}>
-                  //                     {Specialization}
-                  //                 </span>
-                  //             ))
-                  //             ) : (
-                  //                 <span>{ data.Specialization }</span>
-                  //             )
-                  //             }
-                  //         </div>
-                  //         </span>
-                  //         </div>
-                  //         <div class="flex flex-col pl-4 gap-3 justify-center items-center w-[25%]">
-                  //             <span>Bio</span>
-                  //         <span class="pt-2 text-[18px] flex flex-wrap break-words">{ data.bio }</span>
-                  //         </div>
-                  //         {((selectedStatus === 'pending') || (selectedStatus === 'Pending')) && (
-                  //         <div class="flex justify-end w-[25%]">
-                  //             <button autocomplete="off" class="text-base bg-green-500 text-white px-4 py-1 rounded-md ml-4"
-                  //                 onClick={() => handleAccept()}>
-                  //                 Accept
-                  //             </button>
-                  //             <button autocomplete="off" class="text-base bg-red-500 text-white px-4 py-1 rounded-md ml-4"
-                  //                 onClick={() => handleReject()}>
-                  //                 Reject
-                  //             </button>
-
-                  //             {showModal && (
-                  //                 <AcceptDeleteReq
-                  //                     show={showModal}
-                  //                     handleClose={handleCloseModal}
-                  //                     status={updatedStatus}
-                  //                     culId={data.id}
-                  //                     user_id = {data.user_id}
-                  //                 />
-                  //             )}
-                  //         </div>
-                  //         )}
-                  //         {((selectedStatus === 'approved') || (selectedStatus === 'Accepted')) && (
-                  //         <div class="flex justify-end w-[25%]">
-                  //             <button autocomplete="off" class="text-base bg-red-500 text-white px-4 py-1 rounded-md ml-4"
-                  //                 onClick={() => handleReject()}>
-                  //                 Reject
-                  //             </button>
-                  //             {showModal && (
-                  //                 <AcceptDeleteReq
-                  //                     show={showModal}
-                  //                     handleClose={handleCloseModal}
-                  //                     status={updatedStatus}
-                  //                     culId={data.id}
-                  //                     user_id = {data.user_id}
-                  //                 />
-                  //             )}
-                  //         </div>
-                  //         )}
-                  //     </li>
-                  //   ))
                   <p>No data available</p>
                 )}
               </div>
